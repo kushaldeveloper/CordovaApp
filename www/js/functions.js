79cb40decc7817d7328afa9fee327dc92abbc044
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-19 12:38:14 +0530

    test7

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,16 +1,19 @@
 document.addEventListener("deviceready",onDeviceReady,false);
-window.addEventListener("batterystatus", onBatteryStatus, false);
-window.addEventListener("batterylow", onBatteryLow, false);
-window.addEventListener("batterycritical", onBatteryCritical, false);
+//window.addEventListener("batterystatus", onBatteryStatus, false);
+//window.addEventListener("batterylow", onBatteryLow, false);
+//window.addEventListener("batterycritical", onBatteryCritical, false);
 	function onDeviceReady(){
 		alert("device is starting");
 		
+	window.addEventListener("batterystatus", onBatteryStatus, false);	
 	function onBatteryStatus(status) {
 		alert("Level: " + status.level + " isPlugged: " + status.isPlugged);
-    }	
+    }
+    window.addEventListener("batterylow", onBatteryLow, false);	
     function onBatteryLow(status) {
 		alert("Battery Level Low " + status.level + "%");
 	}
+	window.addEventListener("batterycritical", onBatteryCritical, false);
 	function onBatteryCritical(status) {
 		alert("Battery Level Critical " + status.level + "%\nRecharge Soon!");
 	}	
